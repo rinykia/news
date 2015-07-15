e94eda6fe4501a188d168907427d5e979b163fc9
Natali <rinykia@gmail.com>
2015-07-15 11:57:36 +0300

    Update code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
+
   before_action :require_user, except: [:index]
+  
   def index
     @articles = Article.all
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
+
 	def new
 	end
+
 	def create 
     @user = User.find_by_email(params[:session][:email])
     	if @user && @user.authenticate(params[:session][:password])
@@ -9,9 +11,10 @@ class SessionsController < ApplicationController
     	else
       		redirect_to '/login'
     	end
-  	end
-  	def destroy 
-  		session[:user_id] = nil 
-  		redirect_to '/' 
+  end
+
+  def destroy 
+  	session[:user_id] = nil 
+  	redirect_to '/' 
 	end
 end
